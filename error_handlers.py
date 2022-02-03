754e1933a9b951f050b486dfdc1c273e8ec99279
moura-dev <junior.moura19@hotmail.com>
2022-02-03 01:43:34 -0300

    add error_handlers

---
+++ error_handlers.py
@@ -0,0 +1,15 @@
+from flask import jsonify
+
+from apis.app import create_app
+
+app = create_app()
+
+
+@app.errorhandler(404)
+def not_found(e):
+    return jsonify({"Message": "Not Found"}, 404)
+
+
+@app.errorhandler(500)
+def internal_error(e):
+    return {"Message": "Internal Error"}, 500
